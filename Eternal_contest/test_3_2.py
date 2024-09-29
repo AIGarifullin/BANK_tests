ed2c76b36ddfbbadcf875419fa83b135aa8f6893
Adel Garifullin <149618378+AIGarifullin@users.noreply.github.com>
2024-09-29 15:39:12 +0300

    Update test_3_2.py

--- Eternal_contest/test_3_2.py
+++ Eternal_contest/test_3_2.py
@@ -4,16 +4,17 @@
 def min_number_of_stairs_passage(n_t_data: list[int], stairs_data: list[int],
                                  value: int) -> int:
     """Минимальное количество проходов лестницы."""
-    t = n_t_data[1]
-    min_stairs = stairs_data[0]
-    x = stairs_data[value-1]
-    max_stairs = stairs_data[-1]
-    if x - min_stairs <= t:
-        res = max_stairs - min_stairs
-    else:
-        way_1 = 2*(x - min_stairs) + max_stairs - x
-        way_2 = 2*(max_stairs - x) + x - min_stairs
-        res = min(way_1, way_2)
+    n, t = n_t_data[0], n_t_data[1]
+    if 2 <= n and t <= 100: 
+        min_stairs = stairs_data[0]
+        x = stairs_data[value-1]
+        max_stairs = stairs_data[-1]
+        if x - min_stairs <= t:
+            res = max_stairs - min_stairs
+        else:
+            way_1 = 2*(x - min_stairs) + max_stairs - x
+            way_2 = 2*(max_stairs - x) + x - min_stairs
+            res = min(way_1, way_2)
     return res
 
 
